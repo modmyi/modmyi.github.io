2dc1876934ad5842e77eb1ab4848afc87d804c2a
ModMyi <gosia.kanda@gmail.com>
2024-10-29 23:50:48 -0500

    LFS

---
+++ scripts/depictions.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d126e8a8752bb7eb7d0387adf33c199fbc2368fcaeadbba7eb167691b94d39b3
+size 2331
---
+++ scripts/packages_convert.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:99d210de2d27765aad5ec4ae0d28671b077a765578c4a11636cf8b64f19fb852
+size 473
